e735b10da541a85422c5f3385c1262345d586154
Michael Bradley <mjb.bda@gmail.com>
2020-02-06 12:29:39 -0500

    error fixed in custom paragraph css

--- themes/zipdemons-project-theme/inc/block-editor.php
+++ themes/zipdemons-project-theme/inc/block-editor.php
@@ -6,10 +6,11 @@ register_block_style(
         'label'        => esc_html__( 'Blue Quote', 'project-theme' ),
         'inline_style' => '.wp-block-quote.is-style-blue-quote { color: blue; }',
     ),
-    'core/paragraph'
+    'core/paragraph',
     array(
         'name'         => 'center-paragraph',
         'label'        => esc_html__( 'Center Paragraph', 'project-theme' ),
         'inline_style' => '.wp-block-paragraph.is-style-center-paragraph { text-align: center; }',
     )
 );
+?>
