159bfc316d7fda767e93131b09c830c8c792f532
Michael Bradley <mjb.bda@gmail.com>
2020-02-06 12:28:39 -0500

    paragraph custom css

--- themes/zipdemons-project-theme/inc/block-editor.php
+++ themes/zipdemons-project-theme/inc/block-editor.php
@@ -5,5 +5,11 @@ register_block_style(
         'name'         => 'blue-quote',
         'label'        => esc_html__( 'Blue Quote', 'project-theme' ),
         'inline_style' => '.wp-block-quote.is-style-blue-quote { color: blue; }',
+    ),
+    'core/paragraph'
+    array(
+        'name'         => 'center-paragraph',
+        'label'        => esc_html__( 'Center Paragraph', 'project-theme' ),
+        'inline_style' => '.wp-block-paragraph.is-style-center-paragraph { text-align: center; }',
     )
 );
