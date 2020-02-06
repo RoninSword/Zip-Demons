b9174a721126572295b0cb09897ea3b3724b6795
Michael Bradley <mjb.bda@gmail.com>
2020-02-06 11:11:18 -0500

    foundation and what-input code added

--- themes/zipdemons-project-theme/functions.php
+++ themes/zipdemons-project-theme/functions.php
@@ -164,9 +164,8 @@ add_action( 'widgets_init', 'project_theme_widgets_init' );
 function project_theme_scripts() {
 	wp_enqueue_style( 'project-theme-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'project-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'project-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_style( 'project-theme-foundation', get_template_directory_uri() . '/assets/css/vendor/foundation.min.css', null, '6.5.1' );
+	wp_enqueue_script( 'project-theme-what-input', get_template_directory_uri() . '/assets/js/vendor/what-input.js', array( 'jquery' ), '6.5.1', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
