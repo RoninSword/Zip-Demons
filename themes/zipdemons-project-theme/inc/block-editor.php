af8afe169be8c96814b6ba7146ca2b236f9e5b02
Michael Bradley <mjb.bda@gmail.com>
2020-02-07 20:33:41 -0500

    More custom block styling

--- themes/zipdemons-project-theme/inc/block-editor.php
+++ themes/zipdemons-project-theme/inc/block-editor.php
@@ -1,5 +1,4 @@
 <?php
-wp_register_style( 'custom-style', get_template_directory_uri() . '../style.css' );
 register_block_style(
     'core/quote',
     array(
@@ -7,12 +6,17 @@ register_block_style(
         'label'        => esc_html__( 'Blue Quote', 'project-theme' ),
         'inline_style' => '.wp-block-quote.is-style-blue-quote { color: blue; }',
     ),
+    'core/quote',
+    array(
+        'name'         => 'green-quote',
+        'label'        => esc_html__( 'green Quote', 'project-theme' ),
+        'inline_style' => '.wp-block-quote.is-style-purple-quote { color: #5CDB95; }',
+    ),
     'core/paragraph',
     array(
         'name'         => 'center-paragraph',
         'label'        => esc_html__( 'Center Paragraph', 'project-theme' ),
-        'style-handle' => 'custom-style',
-    ),
-
+        'inline_style' => '.wp-block-paragraph.is-style-center-paragraph { text-align: center; }',
+    )
 );
 ?>
