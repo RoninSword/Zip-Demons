83bc20f94d02123eeec921eba2372c72b0660639
AkashKP31 <akashrpatel1999@gmail.com>
2020-02-11 10:06:45 -0500

    header plugin

--- themes/zipdemons-project-theme/header.php
+++ themes/zipdemons-project-theme/header.php
@@ -24,12 +24,12 @@
 <div id="page" class="site">
 
     <header id="masthead" class="site-header">
-    <div class="bg-img">
+    <div class="bg-color">
        
     </div>
         <!-- /*logo */ -->
         <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-            <img src="<?php echo get_template_directory_uri(); ?>./assets/images/logo.png" alt="Logo" width="250px" height="auto" />
+            <!-- <img src="<?php echo get_template_directory_uri(); ?>./assets/images/logo.png" alt="Logo" width="250px" height="auto" /> -->
         </a>
             
   
