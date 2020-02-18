c037bb5299fd9c39329da9ca932a1bc2f8df7271
Arman-Dhaliwal <armandhaliwal98@gmail.com>
2020-02-18 17:38:09 -0500

    first commit

--- themes/zipdemons-project-theme/header.php
+++ themes/zipdemons-project-theme/header.php
@@ -30,6 +30,7 @@
         <!-- /*logo */ -->
         <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
             <img src="<?php echo get_template_directory_uri(); ?>./assets/images/logo.png" alt="Logo" width="250px" height="auto" />
+            <h2>HEllo Arman</h2>
         </a>
             
   
