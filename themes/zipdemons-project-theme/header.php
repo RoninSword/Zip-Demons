7b26e38b8967ea0cefae8a9d97d32584f71c0581
Michael Bradley <mjb.bda@gmail.com>
2020-03-05 11:59:28 -0500

    social media icon fix

--- themes/zipdemons-project-theme/header.php
+++ themes/zipdemons-project-theme/header.php
@@ -46,10 +46,10 @@
     <?php if( get_theme_mod( 'project_theme_facebook_url' ) ||  get_theme_mod( 'project_theme_twitter_url')) { ?>
         <ul class = "social-media">
             <?php if( get_theme_mod( 'project_theme_facebook_url' ) ) { ?>
-                <li class="facebook"><a href="<?php echo get_theme_mod( 'project_theme_facebook_url' ); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/img/facebook-icon.jpg'; ?>" alt="<?php echo esc_html__( "Facebook", 'project_theme' );?>"></a></li>
+                <li class="facebook"><a href="<?php echo get_theme_mod( 'project_theme_facebook_url' ); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/images/facebook-icon.jpg'; ?>" alt="<?php echo esc_html__( "Facebook", 'project_theme' );?>"></a></li>
             <?php } ?>
             <?php if( get_theme_mod( 'project_theme_twitter_url' ) ) { ?>
-                <li class="twitter"><a href="<?php echo get_theme_mod( 'project_theme_twitter_url' ); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/img/twitter.png'; ?>" alt="<?php echo esc_html__( "Twitter", 'project_theme' );?>"></a></li>
+                <li class="twitter"><a href="<?php echo get_theme_mod( 'project_theme_twitter_url' ); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/images/twitter.png'; ?>" alt="<?php echo esc_html__( "Twitter", 'project_theme' );?>"></a></li>
             <?php } ?>
         </ul>
     <?php } ?>
