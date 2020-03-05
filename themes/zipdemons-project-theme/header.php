41d0b70347d6b5214217fc8634da55fd76c1bf78
AkashKP31 <akashrpatel1999@gmail.com>
2020-03-05 11:24:10 -0500

    Merge remote-tracking branch 'origin/develop' into develop

--- themes/zipdemons-project-theme/header.php
+++ themes/zipdemons-project-theme/header.php
@@ -25,9 +25,8 @@
 
     <header id="masthead" class="site-header">
 
-        <!-- /*logo */ -->
+        <!--zzz /*logo */ -->
         <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-            <img class="logo" src="<?php echo get_template_directory_uri(); ?>./assets/images/logo.png" alt="Logo" width="50px" height="50px" />
 </div>
         </a>
             
