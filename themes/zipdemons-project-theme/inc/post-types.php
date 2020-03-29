38ffa27aa52db92d08a86eb7851ecfe3ba7c094e
Michael Bradley <mjb.bda@gmail.com>
2020-03-29 13:33:09 -0400

    post-types.php

--- themes/zipdemons-project-theme/inc/post-types.php
+++ themes/zipdemons-project-theme/inc/post-types.php
@@ -12,3 +12,4 @@ function project_theme_init_post_types() {
         'name'                  => esc_html_x( 'Maintenances', 'Post type general name', 'project_theme' ),
         'singular_name'         => esc_html_x( 'Maintenance', 'Post type singular name', 'project_theme' ),
         'menu_name'             => esc_html_x( 'Maintenances', 'Admin Menu text', 'project_theme' ),
+        'name_admin_bar'        => esc_html_x( 'Maintenance', 'Add New on Toolbar', 'project_theme' ),
