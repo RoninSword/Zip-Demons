199b5efd73cda286c1bfbfdf5f9271e15a449e23
Michael Bradley <mjb.bda@gmail.com>
2020-03-29 14:07:07 -0400

    footer changes

--- themes/zipdemons-project-theme/footer.php
+++ themes/zipdemons-project-theme/footer.php
@@ -1,4 +1,4 @@
-[10:35 a.m., 2020-03-05] Akash Patel: <?php
+<?php
 /**
  * The template for displaying the footer
  *
@@ -6,20 +6,49 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package sample_theme
+ * @package Project_Theme
  */
 
 ?>
 
 	</div><!-- #content -->
 
-	<footer>
+	<?php
+	$args = array(
+		'post_type' => 'sample_theme_maintenance',
+		'post__not_in' => array(
+			get_the_ID(),
+		),
+		'posts_per_page' => 1,
+	);
+
+	$events = new WP_Query( $args );
+
+	if( $events->have_posts() ){
+		while ( $events->have_posts() ){
+			$events->the_post();
+			?>
+			<h2><?php echo get_the_title();?></h2>
+			<?php
+		}
+	}
+	wp_reset_postdata();
+	?>
+
+	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'project_theme' ) ); ?>">
-				
+		<p><?php echo esc_html__( 'Copyright', 'project_theme' ); ?> <?php echo date( 'Y' )?></p>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'project-theme' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'project-theme' ), 'WordPress' );
+				?>
 			</a>
-			<span class="sep">  </span>
-				
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'project-theme' ), 'project-theme', '<a href="https://michael-bradley.net">Michael Bradley</a>' );
+				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
