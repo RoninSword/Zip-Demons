459c22e527502dc775f6adb4de54176d5e6e1ade
Michael Bradley <mjb.bda@gmail.com>
2020-03-29 13:31:49 -0400

    post-type.php

--- themes/zipdemons-project-theme/inc/post-types.php
+++ themes/zipdemons-project-theme/inc/post-types.php
@@ -11,3 +11,4 @@ function project_theme_init_post_types() {
     $labels = array(
         'name'                  => esc_html_x( 'Maintenances', 'Post type general name', 'project_theme' ),
         'singular_name'         => esc_html_x( 'Maintenance', 'Post type singular name', 'project_theme' ),
+        'menu_name'             => esc_html_x( 'Maintenances', 'Admin Menu text', 'project_theme' ),
